8e8ede488018bd95a7cb26c8fddbc99c5a412e73
Jordan Seiler <jorsei1p217@gmail.com>
2024-05-27 15:45:42 -0400

    Remove path prefixing
    Not needed if you have a domain, which I do

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const katexRendering = require("@iktakahiro/markdown-it-katex");
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(katexRendering));
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
