5eb9db426a9ef4386168cae19165c3b5358068f6
JDSeiler <jorsei1p217@gmail.com>
2025-01-15 17:34:57 -0500

    Add configuration for RSS plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const katexRendering = require("@iktakahiro/markdown-it-katex");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "post",
+      limit: 10,
+    },
+    metadata: {
+      language: "en",
+      title: "Seiler's Corner",
+      subtitle: "Weblog of Jordan Seiler",
+      base: "https://jordanseiler.xyz/blog/",
+      author: {
+        name: "Jordan Seiler",
+      },
+    },
+  });
   eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(katexRendering));
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
@@ -11,6 +29,6 @@ module.exports = function(eleventyConfig) {
     // input and output directories
     dir: {
       input: "src",
-    }
+    },
   };
 };
